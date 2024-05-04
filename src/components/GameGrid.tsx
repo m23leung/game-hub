48f0d95289251729e8a33cbaf41188a3b85c2ca6
m23leung <mark.leung89@gmail.com>
2024-05-04 21:17:51 +1000

    Refactor logic for isLoading game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -23,17 +23,17 @@ const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
         spacing={3}
         padding={10}
       >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+        {isLoading
+          ? skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            ))
+          : data.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
       </SimpleGrid>
     </>
   );
